da7cb08d0f286190ade08cda30dffce758bc5830
yanyige <232218719@qq.com>
2021-03-11 23:06:16 +0800

    feat: finish simple factory example

---
+++ src/main/java/com/alibaba/dataworks/design/factory/sinplefactory/factorystore/OrderPizza.java
@@ -0,0 +1,39 @@
+package com.alibaba.dataworks.design.factory.sinplefactory.factorystore;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.alibaba.dataworks.design.factory.sinplefactory.pizzastore.pizza.Pizza;
+
+/**
+ * @author yige.yyg (奕格)
+ * @date 2021/3/11
+ */
+public class OrderPizza {
+
+    SimpleFactory simpleFactory;
+
+    public void setFactory(SimpleFactory simpleFactory) {
+        String orderType = "";
+
+        this.simpleFactory = simpleFactory;
+        Pizza pizza;
+
+        while (true) {
+            orderType = getType();
+            pizza = simpleFactory.createPizza(orderType);
+        }
+    }
+
+    private String getType() {
+        try {
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("please input pizza type:");
+            return stdin.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
--- src/main/java/com/alibaba/dataworks/design/factory/sinplefactory/factorystore/SimpleFactory.java
+++ src/main/java/com/alibaba/dataworks/design/factory/sinplefactory/factorystore/SimpleFactory.java
@@ -1,9 +1,30 @@
 package com.alibaba.dataworks.design.factory.sinplefactory.factorystore;
 
+import com.alibaba.dataworks.design.factory.sinplefactory.pizzastore.pizza.ChessPizza;
+import com.alibaba.dataworks.design.factory.sinplefactory.pizzastore.pizza.GreekPizza;
+import com.alibaba.dataworks.design.factory.sinplefactory.pizzastore.pizza.Pizza;
+
 /**
+ * 简单工厂模式
+ *
  * @author yige.yyg (奕格)
  * @date 2021/3/10
  */
 public class SimpleFactory {
+    public Pizza createPizza(String orderType) {
 
+        System.out.println("使用简单工厂模式");
+
+        Pizza pizza = null;
+
+        if ("greek".equals(orderType)) {
+            pizza = new GreekPizza();
+            pizza.setName("greek");
+        } else if ("cheese".equals(orderType)) {
+            pizza = new ChessPizza();
+            pizza.setName("cheese");
+        }
+
+        return pizza;
+    }
 }
